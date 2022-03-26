09103bf7a9f5eb0e7a5cb02128595d9fe81ef27d
flyboy85749 <freedmouse@gmail.com>
2022-03-26 09:48:26 -0700

    add table

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import "./App.css";
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import BarChart from "./components/BarChart";
-import {LanguageData}  from './Data'
+import Table from "./components/Table";
+import { LanguageData } from "./Data";
 
 const App = () => {
-
   const [languageData, setLanguageData] = useState({
     labels: LanguageData.map((data) => data.name),
-    datasets: [{
-      label: 'Popularity of Programming Languages',
-      data: LanguageData.map((data) => data.avgSalary),
-backgroundColor: ['#9896D9', '#8F8EBF', '#A0D9CE', '#D9D4D0', '#F29188']
-    }]
-  })
+    datasets: [
+      {
+        label: "Popularity of Programming Languages by Salary",
+        data: LanguageData.map((data) => data.avgSalary),
+        backgroundColor: [
+          "#9896D9",
+          "#8F8EBF",
+          "#A0D9CE",
+          "#D9D4D0",
+          "#F29188",
+        ],
+        borderColor: "gray",
+        borderWidth: 1,
+
+      },
+    ],
+    
+  });
+
+
   return (
     <div>
-      <div style={{width: 700}}>
-      <BarChart chartData={languageData}/>
+      <div style={{ width: 700 }}>
+        <BarChart chartData={languageData} />
+      </div>
+      <div>
+        <Table />
       </div>
     </div>
   );
